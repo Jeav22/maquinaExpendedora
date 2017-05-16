eb11a01670c9da50b757c863a550ea4fbdb81f6e
TORRE <TORRE@TORRE-PC>
2017-05-15 19:56:08 -0500

    Creacion de clases Usuario 2

--- src/main/java/co/usa/edu/modelo/UsuarioAdministrativo.java
+++ src/main/java/co/usa/edu/modelo/UsuarioAdministrativo.java
@@ -2,4 +2,14 @@ package co.usa.edu.modelo;
 
 public class UsuarioAdministrativo {
 
+	String contrasena;
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
 }
---
+++ src/main/java/co/usa/edu/modelo/UsuarioComprador.java
@@ -0,0 +1,14 @@
+package co.usa.edu.modelo;
+
+public class UsuarioComprador {
+
+	int SaldoUsuario;
+
+	public int getSaldoUsuario() {
+		return SaldoUsuario;
+	}
+
+	public void setSaldoUsuario(int saldoUsuario) {
+		SaldoUsuario = saldoUsuario;
+	}
+}
