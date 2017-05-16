3dbaae30d3515424c1f0321a90028c9cc8f05fd9
TORRE <TORRE@TORRE-PC>
2017-05-15 20:30:13 -0500

    Clases Administrador para ambos usuarios

--- src/main/java/co/usa/edu/modelo/AdministradorUsuarioAdministrativo.java
+++ src/main/java/co/usa/edu/modelo/AdministradorUsuarioAdministrativo.java
@@ -1,5 +1,11 @@
 package co.usa.edu.modelo;
 
 public class AdministradorUsuarioAdministrativo {
+	
+	public UsuarioAdministrativo usuarioadministrativo;
+	
+	public AdministradorUsuarioAdministrativo(){
+		usuarioadministrativo = new UsuarioAdministrativo();
+	}
 
 }
--- src/main/java/co/usa/edu/modelo/AdministradorUsuarioComprador.java
+++ src/main/java/co/usa/edu/modelo/AdministradorUsuarioComprador.java
@@ -1,5 +1,11 @@
 package co.usa.edu.modelo;
 
 public class AdministradorUsuarioComprador {
+	
+	public UsuarioComprador usuariocomprador;
+	
+	public AdministradorUsuarioComprador(){
+		usuariocomprador = new UsuarioComprador();
+	}
 
 }
