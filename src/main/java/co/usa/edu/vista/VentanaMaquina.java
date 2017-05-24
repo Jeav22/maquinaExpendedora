a8f7b0c9a413dcc0c1097aa197573bc9e45000d4
Jeav22 <jorge.arenas@correo.usa.edu.co>
2017-05-24 08:45:31 -0500

    creacion de vistas (panels) y servicios

--- src/main/java/co/usa/edu/vista/VentanaMaquina.java
+++ src/main/java/co/usa/edu/vista/VentanaMaquina.java
@@ -27,7 +27,24 @@ public class VentanaMaquina extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        menu = new javax.swing.JMenuBar();
+        sesion = new javax.swing.JMenu();
+        iniciarAdministrador = new javax.swing.JMenuItem();
+        cerrarAdministrador = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        sesion.setText("Sesion");
+
+        iniciarAdministrador.setText("Iniciar Administrador");
+        sesion.add(iniciarAdministrador);
+
+        cerrarAdministrador.setText("Cerrar Administrador");
+        sesion.add(cerrarAdministrador);
+
+        menu.add(sesion);
+
+        setJMenuBar(menu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -37,7 +54,7 @@ public class VentanaMaquina extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +96,9 @@ public class VentanaMaquina extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem cerrarAdministrador;
+    private javax.swing.JMenuItem iniciarAdministrador;
+    private javax.swing.JMenuBar menu;
+    private javax.swing.JMenu sesion;
     // End of variables declaration//GEN-END:variables
 }
